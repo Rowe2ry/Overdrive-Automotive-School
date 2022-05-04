7d573d2cca4bdcfda54ddc216c11bae564720eb1
Chris Rowe <rowe2ry@yahoo.com>
2022-05-04 10:21:53 -0400

    basic get request set up

--- controllers/controllerLogic/homeLogic.js
+++ controllers/controllerLogic/homeLogic.js
@@ -1,7 +1,6 @@
 const renderHomePage = async (req,res)=> {
     try {
-        res.status(200).json('This is working');
-        // const homePage = await res.render('home');
+        const homePage = await res.render('home');
     } catch (err) {
         res.status(400).json(err);
     };
