5250f4cc465dacb7586c40bc096a97e8365c5a5e
Chris Rowe <rowe2ry@yahoo.com>
2022-05-09 17:12:24 -0400

    Updating lessons and lasson layout.

---
+++ seeds/update_lessons.js
@@ -0,0 +1,16 @@
+const seedLessons = require('./seedLessons');
+const seedQuizzes = require('./seedQuizzes');
+
+const sequelize = require('../config/connection');
+
+const seedDb = async () => {
+    await sequelize.sync({ force:false });
+    console.log('\n ========== DATABASE ✅ ========== \n');
+    await seedLessons();;
+    console.log('\n ========== LESSONS ✅ ========== \n');
+    await seedQuizzes();;
+    console.log('\n ========== QUIZZES ✅ ========== \n');
+    process.exit(0);
+};
+
+seedDb();
