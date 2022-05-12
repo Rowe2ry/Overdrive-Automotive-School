95d65375d6d3e5789ac0cf1e27ab07bf5c23e80d
Chris Rowe <rowe2ry@yahoo.com>
2022-05-11 23:14:45 -0400

    trying again to drop just lessons and quiz tables for seed updating part 6

--- seeds/update_lessons.js
+++ seeds/update_lessons.js
@@ -7,16 +7,16 @@ const sequelize = require('../config/connection');
 const seedDb = async () => {
     await sequelize.sync({ force:false });
     console.log('\n ========== DATABASE ✅ ========== \n');
+    await Quiz.sync({ force: true });
+    console.log('\n ========== RECREATED QUIZZES ✅ ========== \n');
     await Comment.sync({ force: true });
     console.log('\n ========== RECREATED COMMENTS ✅ ========== \n');
     await Lesson.sync({ force: true });
     console.log('\n ========== RECREATED LESSONS ✅ ========== \n');
-    await Quiz.sync({ force: true });
-    console.log('\n ========== RECREATED QUIZZES ✅ ========== \n');
     await seedLessons();;
-    console.log('\n ========== LESSONS ✅ ========== \n');
+    console.log('\n ========== SEEDED LESSONS ✅ ========== \n');
     await seedQuizzes();;
-    console.log('\n ========== QUIZZES ✅ ========== \n');
+    console.log('\n ========== SEEDED QUIZZES ✅ ========== \n');
     process.exit(0);
 };
 
